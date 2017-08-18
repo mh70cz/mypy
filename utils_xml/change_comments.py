8a521745272511b36147c25a457b3565fc59ef2f
mh70 <mh70@mh70.cz>
2017-08-19 00:13:17 +0200

    works on Linux, + custom xml_formater

--- utils_xml/change_comments.py
+++ utils_xml/change_comments.py
@@ -1,9 +1,5 @@
-"""  xxx """
+"""  konvertuje obsah elemntů xs:documentation bez xml:lang do komentářů """
 from lxml import etree
-import xmlformatter
-
-os_type = "Win" # Win | Linux
-
 
 f_name_in = "TradeRegisterListRequest_orig.xsd"
 f_name_out = "TradeRegisterListRequest.xsd"
@@ -19,48 +15,44 @@ def main():
         documentations = annotation.findall("./xs:documentation", namespaces)
         for documentation in documentations:
             att = documentation.attrib
-            if att.get(xml_lang, None) == "cs":
+            if att.get(xml_lang, None) in ["cs", "en", "sk"]:
                 # print(documentation.text)
                 pass
-            elif att.get(xml_lang, None) == "en":
-                #print(documentation.text)
-                pass
+
             elif att.get(xml_lang, None) is None:
                 txt = documentation.text
                 comment = etree.Comment(txt)
-                annotation.insert(0, comment)
                 documentation.getparent().remove(documentation)
                 # print("delelted: " + str(txt))
+                #annotation.insert(0, comment)
+                annotation.append(comment)
 
+    rough_bin_string = etree.tostring(root, encoding="utf-8",
+                                      xml_declaration=True, pretty_print=True)
 
-    #tree.write(open('output.xml', 'wb'))
-    tree.write(open(f_name_out, 'wb'), encoding='utf-8', xml_declaration=True, pretty_print=True)
+    format_xml(rough_bin_string)
 
-def add_nl():
-    """ přidej odřádkování za '-->'  """
-    if os_type == "Linux":
-        with open('output.xml') as f:
-            read_data = f.read()
+    # with open(f_name_out, "wb") as wf:
+    #     wf.write(rough_bin_string)
+    #     #tree.write(open('output.xml', 'wb'))
+    #tree.write(open(f_name_out, 'wb'), encoding='utf-8', xml_declaration=True, pretty_print=True)
 
-        write_data = read_data.replace("--><", "-->\r<")
-
-        with open("output_ref.xml", "w") as wf:
-            wf.write(write_data)
-    elif os_type == "Win":
-        with open(f_name_out, encoding="utf-8") as f:
-            read_data = f.read()
-
-        write_data = read_data.replace("--><", "-->\r<")
-
-        with open(f_name_out, "w", encoding="utf-8") as wf:
-            wf.write(write_data)
-
-
-def reformat():
-    """ formatuj xml """
-    formatter = xmlformatter.Formatter(indent="2", indent_char=" ")
-    formatter.format_file(f_name_out)
+def format_xml(xml_bin_string):
+    """ přidání Comment elementu do xs:annotation nepřidá nový řádek
+    tato procedura doformátuje a zapíše do souboru"""
+    output = ""
+    lenght = 0
+    s = xml_bin_string.decode("utf-8")
+    s = s.replace("--><", "-->\n<")
+    s = s.split("\n")
+    for line in s:
+        #print(line)
+        if "<xs:annotation>" in line:
+            lenght = len(line) - 15
+        elif ("</xs:annotation>" in line) and (len(line) < 19):
+            line = str(lenght * " ") + line
+        output += line + "\n"
+    with open(f_name_out, "w", encoding="utf-8") as wf:
+        wf.write(output)
 
 main()
-add_nl()
-reformat()

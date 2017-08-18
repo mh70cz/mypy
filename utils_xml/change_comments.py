6060c45dd6f185b93502c6d11913b715cf02b8c8
CIS\m.houska <m.houska@creditinfosolutions.com>
2017-08-18 16:46:26 +0200

    tested in windows

--- utils_xml/change_comments.py
+++ utils_xml/change_comments.py
@@ -1,8 +1,15 @@
-import lxml.etree as et
+"""  xxx """
+from lxml import etree
+import xmlformatter
 
+os_type = "Win" # Win | Linux
+
+
+f_name_in = "TradeRegisterListRequest_orig.xsd"
+f_name_out = "TradeRegisterListRequest.xsd"
 
 def main():
-    tree = et.parse('TradeRegisterListRequest.xsd')
+    tree = etree.parse(f_name_in)
     root = tree.getroot()
     namespaces = {'xs': 'http://www.w3.org/2001/XMLSchema',}
     annotations = root.findall(".//xs:annotation", namespaces)
@@ -20,26 +27,40 @@ def main():
                 pass
             elif att.get(xml_lang, None) is None:
                 txt = documentation.text
-                comment = et.Comment(txt)
+                comment = etree.Comment(txt)
                 annotation.insert(0, comment)
                 documentation.getparent().remove(documentation)
                 # print("delelted: " + str(txt))
-                pass
-
 
 
     #tree.write(open('output.xml', 'wb'))
-    tree.write(open('output.xml', 'wb'), encoding='utf-8', xml_declaration=True, pretty_print=True)
+    tree.write(open(f_name_out, 'wb'), encoding='utf-8', xml_declaration=True, pretty_print=True)
+
+def add_nl():
+    """ přidej odřádkování za '-->'  """
+    if os_type == "Linux":
+        with open('output.xml') as f:
+            read_data = f.read()
+
+        write_data = read_data.replace("--><", "-->\r<")
+
+        with open("output_ref.xml", "w") as wf:
+            wf.write(write_data)
+    elif os_type == "Win":
+        with open(f_name_out, encoding="utf-8") as f:
+            read_data = f.read()
+
+        write_data = read_data.replace("--><", "-->\r<")
+
+        with open(f_name_out, "w", encoding="utf-8") as wf:
+            wf.write(write_data)
+
 
 def reformat():
-    with open('output.xml') as f:
-        read_data = f.read()
-  
-    write_data = read_data.replace("--><", "-->\r<")
-
-    with open("output_ref.xml", "w") as wf:
-        wf.write(write_data)
+    """ formatuj xml """
+    formatter = xmlformatter.Formatter(indent="2", indent_char=" ")
+    formatter.format_file(f_name_out)
 
 main()
+add_nl()
 reformat()
-

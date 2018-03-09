b0f9932c828027807604a7f074f0d2812e60f707
mh70cz <mh70@mh70.cz>
2018-03-09 21:53:28 +0100

    selenium interactive

---
+++ spyder/first_snow/selenium_interactive_1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+http://selenium-python.readthedocs.io
+https://realpython.com/blog/python/modern-web-automation-with-python-and-selenium/
+
+dokumentace s příklady:
+https://media.readthedocs.org/pdf/selenium-python/latest/selenium-python.pdf
+    
+"""
+
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+opts = Options()
+opts.set_headless(False)
+browser = Firefox(options=opts)
+browser.get('https://www.ihned.cz')
+
+x = browser.find_elements_by_class_name('article-title')
+y = x[0].find_element_by_tag_name("a")
+y.click()
+
+browser.get_screenshot_as_file('./scrsht/img_1.png')
+
+browser.set_window_size(800,2048)
+
+browser.get_screenshot_as_file('./scrsht/img_2.png')
+
+browser.minimize_window()
+browser.maximize_window()
+
+browser.back()
+browser.refresh()
+
+
+#zavři cookie warning
+cookies = browser.get_cookies()
+cookies_old = cookies.copy()
+
+#umožňuje zadat jen 1 class
+cookie_warning_1 = browser.find_element_by_class_name('cc_btn_accept_all')
+
+#umožňuje zadat více classes (.cc_btn and .cc_btn_accept_all)
+cookie_warning_2 = browser.find_element_by_css_selector(
+        '.cc_btn.cc_btn_accept_all')
+
+cookie_warning_2.click() #clik for close warning banner
+
+#najdi nově přidanou cookie
+cookies = browser.get_cookies()
+[c['name'] for c in cookies if c not in cookies_old]
+
+browser.delete_cookie('cookieconsent_dismissed') 
+

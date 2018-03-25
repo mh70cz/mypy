149b3b294efedf17d5b516439805f81ed849e09c
mh70cz <mh70@mh70.cz>
2018-03-25 01:07:59 +0100

    pybites 5, 8 + pd skip row

--- challenges/pybites/bite_05.py
+++ challenges/pybites/bite_05.py
@@ -14,7 +14,7 @@ def dedup_and_title_case_names(names):
 def sort_by_surname_desc(names):
     """Returns names list sorted desc by surname"""
     names = dedup_and_title_case_names(names)
-    return sorted(names, key = lambda x: x.split(" ")[1], reverse = True)
+    return sorted(names, key=lambda x: x.split(" ")[1], reverse = True)
 
 
 def shortest_first_name(names):
@@ -22,22 +22,21 @@ def shortest_first_name(names):
     names = dedup_and_title_case_names(names)
     # return sorted(names, key = lambda x: x.split(" ")[0])[0].split(" ")[0]
     return sorted([name.split(" ")[0] for name in names],
-                   key = lambda x: len(x))[0]
+                  key=lambda x: len(x))[0]
 
 
 
-names = dedup_and_title_case_names(NAMES)
-assert names.count('Bob Belderbos') == 1
-assert names.count('julian sequeira') == 0
-assert names.count('Brad Pitt') == 1
-assert len(names) == 10
-assert all(n.title() in names for n in NAMES)
+nam = dedup_and_title_case_names(NAMES)
+assert nam.count('Bob Belderbos') == 1
+assert nam.count('julian sequeira') == 0
+assert nam.count('Brad Pitt') == 1
+assert len(nam) == 10
+assert all(n.title() in nam for n in NAMES)
 
 
 
-names = sort_by_surname_desc(NAMES)
-assert names[0] == 'Julian Sequeira'
-assert names[-1] == 'Alec Baldwin'
-
+nam = sort_by_surname_desc(NAMES)
+assert nam[0] == 'Julian Sequeira'
+assert nam[-1] == 'Alec Baldwin'
 
 assert shortest_first_name(NAMES) == 'Al'

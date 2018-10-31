ad80c88843f9c5099712a8055afefebc55aba0f1
CIS\m.houska <m.houska@creditinfosolutions.com>
2018-10-31 19:17:41 +0100

    parametrize tests

--- challenges/own/cheese_wine/gourmets_nightmare_test.py
+++ challenges/own/cheese_wine/gourmets_nightmare_test.py
@@ -2,11 +2,22 @@ import pytest
 from gourmets_nightmare import best_match_per_wine, match_wine_5cheeses
 
 
-def test_best_match_per_wine():
+def test_best_match_per_wine_all():
     assert best_match_per_wine()[2] == 13.0
-    assert best_match_per_wine("white")[2] == 8.0
-    assert best_match_per_wine("red")[2] == 13.0
-    assert best_match_per_wine("sparkling")[2] == 6.0
+
+
+cases_best_by_wine = [
+    ("white", "Sauvignon blanc", "Smoked Austrian", 8.0),
+    ("red", "Cabernet sauvignon", "Dorset Blue Vinney", 13.0),
+    ("sparkling", "Moscato d’Asti", "Carré de l'Est", 6.0),
+]
+
+
+@pytest.mark.parametrize("case", cases_best_by_wine)
+def test_best_match_per_wine_type(case):
+    wine_type, *result = case
+    assert best_match_per_wine(wine_type)[:3] == tuple(result)
+    # assert best_match_per_wine(wine_type) == tuple(result)
 
 
 def test_invalid_wine_type():
@@ -31,7 +42,7 @@ cases = [
 @pytest.mark.parametrize("case", cases)
 def test_match_wine_5cheeses(case):
     """ test of presence of first 2 cheeses only
-    because if score is same for more pairs order of pairs cannot be assumed
+    because if score is same for more pairs, order of pairs cannot be assumed
     """
     idx, wine, cheese1, cheese2 = case
     assert mw5c[idx][0] == wine

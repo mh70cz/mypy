96789f8737fa356a3594db544cdb14d388b962c7
mh70cz <mh70@mh70.cz>
2019-04-30 16:51:00 +0200

    .

--- spyder/autumn_walks/nltk/nltk_1.py
+++ spyder/autumn_walks/nltk/nltk_1.py
@@ -10,10 +10,14 @@ Created on Mon Apr 29 12:39:09 2019
 
 
 import nltk
-sentence = "Ve snaze o podporu znalostní ekonomiky, odchod od ekonomiky montoven a konec levné práce se vláda rozhodla dodatečně zdanit dva nejpokročilejší sektory naší ekonomiky, finanční služby a telco."
+sentence_ = "Ve snaze o podporu znalostní ekonomiky, odchod od ekonomiky montoven a konec levné práce se vláda rozhodla dodatečně zdanit dva nejpokročilejší sektory naší ekonomiky, finanční služby a telco."
+sentence = "Bring us food, or be food yourself."
 tokens = nltk.word_tokenize(sentence)
 
 tokens
 tagged = nltk.pos_tag(tokens)
 
-tagged
+article = """Ve stavbě letadel byli pak relativně úspěšní bratři Eugen a Hugo Čihákovi, mimo jiné bratranci Kašpara. Jejich nejpovedenější stroj se zrodil koncem roku 1912, nesl jméno Rapid a ne úplnou náhodou svými tvary připomínal francouzské stroje Morane-Saulnier. Rapid se vlastně stal i nejúspěšnější leteckou konstrukcí do roku 1914 u nás.
+"""
+
+tokens_sen = nltk.sent_tokenize(article)

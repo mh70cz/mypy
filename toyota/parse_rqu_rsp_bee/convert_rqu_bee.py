6f8f6d0e2b6b830f309de4d9e10e6489bfdf22c8
mh70cz <mh70@mh70.cz>
2019-11-24 22:54:39 +0100

    .

--- toyota/parse_rqu_rsp_bee/convert_rqu_bee.py
+++ toyota/parse_rqu_rsp_bee/convert_rqu_bee.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 @author: mh70 , Created on Wed Nov 20 20:55:30 2019 
+provede konverzi z logu requestu do BEE do formatu, ktery lze pouzit v primo v BEE
+napr. Batch testing , nebo nakopirovat do Testing Data v projektu
+Konverze zahrnuje:
+vyber pouze elementu In ; 2x oprava ns ; konverze z utf-16le do  utf-8
+
+
+https://stackoverflow.com/questions/13412496/python-elementtree-module-how-to-ignore-the-namespace-of-xml-files-to-locate-ma
+https://stackoverflow.com/questions/22039115/python-specify-xmlns-on-xml-etree-elements
+https://stackoverflow.com/questions/8983041/saving-xml-files-using-elementtree 
 """
 
 import xml.etree.ElementTree as ET
@@ -8,8 +17,14 @@ from pathlib import Path
 import os
 
 
-srcpath = Path("C:/BEE_rqu/rqu_BEE_Live_spo")
-dstpath = Path("C:/BEE_batch/in_spo")
+#srcpath = Path("C:/BEE_rqu/rqu_BEE_Live_spo")
+#dstpath = Path("C:/BEE_batch/in_spo")
+
+#srcpath = Path("C:/BEE_rqu/rqu_BEE_Live_fop")
+#dstpath = Path("C:/BEE_batch/in_fop")
+
+srcpath = Path("C:/BEE_rqu/rqu_BEE_Live_po")
+dstpath = Path("C:/BEE_batch/in_po")
 
 ns = {"cls":"uri:creditinfosolutions/cls",
       "nrki":"nrki_ToDo",
@@ -17,12 +32,17 @@ ns = {"cls":"uri:creditinfosolutions/cls",
       'gr': 'urn:crif-cribiscz-GetGlobalReport:2013-05-03',
       "types": "http://isirws.cca.cz/types/"}
 
-ET.register_namespace('cls',"uri:creditinfosolutions/cls")
+# prida do elemnetu attrib xmlns="uri:creditinfosolutions/cls" , neni pouzit prefix cls:
+ET.register_namespace('',"uri:creditinfosolutions/cls")
+# pouzije prefix cls:
+#ET.register_namespace('cls',"uri:creditinfosolutions/cls")
+
 ET.register_namespace('nrki',"nrki_ToDo")
 ET.register_namespace("types", "http://isirws.cca.cz/types/")
 ET.register_namespace("urp", "https://ws.urplus.sk")
 ET.register_namespace("cee", "urn:crif-cribiscz-ExekuceGet:2013-01-10")
 ET.register_namespace("gr", "urn:crif-cribiscz-GetGlobalReport:2013-05-03")
+# 
 
 
 def get_root(path):
@@ -46,19 +66,25 @@ for file in os.listdir(srcpath):
         files.append(file)
 
 data = []
-for file in files[:5]:
+for file in files[-10:]:
     
     path = srcpath / file
     print(path)
     root = get_root(path)
     
     in_elem = root.find(".//cls:In", ns)
-    in_elem.attrib["xmlns"] = "uri:creditinfosolutions/cls"
     
+    # ET.register_namespace('',"uri:creditinfosolutions/cls") zajisti pridani 
+    # xmlns="uri:creditinfosolutions/cls" attributu do elementu, nasleduji radek
+    # by zpusobil duplicitni vyskyt tohoto attributu
+    #in_elem.attrib["xmlns"] = "uri:creditinfosolutions/cls"
+    
+    
+    # oprava 2 chybnych ns -- poresit s Petrem J.
     if in_elem is not None:
         cr_rep_elem = in_elem.find(".//CR_REP", ns)
         if cr_rep_elem is not None:
-            cr_rep_elem.attrib["xmlns"] = "nrki_ToDo"
+            cr_rep_elem.attrib["xmlns"] = "nrki_ToDo" 
             
         conn_isir_elem = in_elem.find(".//cls:ConnectorIsir", ns)
         if conn_isir_elem is not None:
@@ -69,21 +95,12 @@ for file in files[:5]:
         
 
     
-    out_tree = ET.ElementTree(in_elem)
+    out_tree = ET.ElementTree(in_elem) # vytvor novy tree 
     path_out = dstpath / file
-    #out_tree.write(path_out, encoding="utf-16le")
+    #out_tree.write(path_out, encoding="utf-16le")  # BEE neakceptuje(?)
     out_tree.write(path_out, encoding="utf-8")                
     
-        
-        
-        
-#    nrki = subj_elem.find(".//cls:ConnectorNrki", ns)
-#    if nrki is None:
-#        return None
-#    
-#    if (nrki.attrib["status"] != "ok") and (nrki.attrib["cnavStatus"] != "100"):
-#        return None
-    
+            
     
 """
 in_elem.attrib["xmlns"] = "uri:creditinfosolutions/cls"
@@ -92,7 +109,7 @@ in_elem.tag = "{uri:aaa}In"
 
 
 out_tree = ET.ElementTree(in_elem)
-ET.register_namespace('cls',"uri:creditinfosolutions/cls")
+ET.register_namespace('',"uri:creditinfosolutions/cls")
 ET.register_namespace('nrki',"nrki_ToDo")
 ET.register_namespace("types", "http://isirws.cca.cz/types/")
 out_tree.write("C:/BEE_batch/in_spo/bflm.xml", encoding="utf-16le")
